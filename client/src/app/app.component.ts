ec26d111b2f4eaea6cecea5f9f22004867a64e2c
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-07-21 09:44:15 +0530

    Header,Footer,Carousel Completed

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, AfterViewInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements AfterViewInit {
+  isNavbarActive: boolean = false;
+
+  ngAfterViewInit(): void {
+    this.initializeCarousel();
+  }
+
+  toggleNavbar() {
+    this.isNavbarActive = !this.isNavbarActive;
+  }
+
+  initializeCarousel(): void {
+    $('#carouselExampleIndicators').carousel({
+      interval: 1500, // Set the interval to 3 seconds (adjust as needed)
+      pause: 'hover' // Pause on hover
+    });
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+// app.module.ts
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
